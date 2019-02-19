a2733698e91e201953d31dac28eca376fdba0405
MilenIvanovv <milenivanovv@gmail.com>
2019-02-19 19:07:15 +0200

    removed new Header

--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 // Write you code here, feel free to add new js files and include the in index.html script tags
 // Create a new user
 
-var myHeaders = new Headers();
 
 
 function AddNewUser() {
@@ -16,12 +15,10 @@ function AddNewUser() {
         lastName: document.getElementById("lastName").value,
         })
     })
-
 }
 
 
 function LoginUser() {
-
 
     fetch('http://localhost:3000/auth/login', {
     headers: { "Content-Type": "application/json; charset=utf-8" },
@@ -33,14 +30,14 @@ function LoginUser() {
     }).then((response) => {
         return(response.json());   
     }).then((responseJson) => {
-        if(responseJson.success){
-            window.localStorage.setItem("token",responseJson.token);
-            myHeaders.append("authorization",responseJson.token);
-            console.log(myHeaders.get("authorization"));
-            alert("Logged in"); 
-        }else {
+        if(!responseJson.success){
             alert("Wrong username or password");   
+            return;
         }
+        window.localStorage.setItem("token",responseJson.token);
+        myHeaders.append("authorization",responseJson.token);
+        console.log(myHeaders.get("authorization"));
+        alert("Logged in"); 
     });
 
 }
